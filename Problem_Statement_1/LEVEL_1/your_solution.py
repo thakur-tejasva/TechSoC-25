cc3529a55efa5e9329a731ca1903ae9843d7f8de
Thakur Tejasva Singh <tejasva.singh.thakur@outlook.com>
2025-08-20 20:14:09 +0530

    Added test case as a poem

--- Problem_Statement_1/LEVEL_1/your_solution.py
+++ Problem_Statement_1/LEVEL_1/your_solution.py
@@ -1,10 +1,18 @@
 #PS 1, Caesar Cipher
-str=input("Input string(can handle capital, small, space, special characters): ")
-sft=int(input("Enter Shift (<+shift> for encode, <-shift for decode>): "))
+#Test case or custom input
+res=input("Do you want to proceed with test case(a poem) with shift 3?(y or anything), or custom inut(n or N) (y/n)").lower()
+if res=="n":
+    str=input("Input string(can handle capital, small, space, special characters): ")
+    sft=int(input("Enter Shift (<+shift> for encode, <-shift for decode>): "))
+else:
+    with open(test_cases.txt) as f:
+        str=f.read()
+    sft= 3
 #Modular arithematic is used for cyclicity of functions
 def encoding(x):
     if x.isupper() : return chr(65+(ord(x)-65+sft)%26)
     if x.islower() : return chr(97+(ord(x)-97+sft)%26)
     else: return x
 enc="".join(map(encoding, str))
+print("Original string is:\n", str)
 print("Encoded Result is:\n ", enc)
